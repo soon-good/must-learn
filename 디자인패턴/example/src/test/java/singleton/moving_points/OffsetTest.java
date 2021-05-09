fe6f561e88af6cd3040845d18fea2b06d4bd8807
sgjung <playmymind@daum.net>
2021-05-10 08:49:39 +0900

    싱글턴 패턴 > 테스트 코드 추가 및 문서 수정

--- 디자인패턴/example/src/test/java/singleton/moving_points/OffsetTest.java
+++ 디자인패턴/example/src/test/java/singleton/moving_points/OffsetTest.java
@@ -1,6 +1,7 @@
 package singleton.moving_points;
 
 import io.study.design_pattern.singleton.moving_points.Offset;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OffsetTest {
@@ -9,15 +10,19 @@ public class OffsetTest {
 	void 상하좌우_인스턴스테스트(){
 		Offset top = Offset.top();
 		System.out.println("top = " + top);
+		Assertions.assertSame(top, Offset.top());
 
 		Offset bottom = Offset.bottom();
 		System.out.println("bottom = " + bottom);
+		Assertions.assertSame(bottom, Offset.bottom());
 
 		Offset left = Offset.left();
 		System.out.println("left = " + left);
+		Assertions.assertSame(left, Offset.left());
 
 		Offset right = Offset.right();
 		System.out.println("right = " + right);
+		Assertions.assertSame(right, Offset.right());
 
 	}
 }
