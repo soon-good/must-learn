156ee1488a89583d6aa5c4a1a1f3145f387d1687
gosgjung <playmymind@daum.net>
2021-07-24 20:36:44 +0900

    모던자바 정리용도 프로젝트 추가 : 의존성을 이것저것 추가하기 귀찮아서 왠만한 필수적인 대부분의 것들을 갖춘 스프링 부트로 프로젝트를 생성...

---
+++ 모던자바/modern-java-example/src/main/java/io/study/modernjavaexample/ModernJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package io.study.modernjavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaExampleApplication.class, args);
+	}
+
+}
---
+++ 모던자바/modern-java-example/src/test/java/io/study/modernjavaexample/ModernJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.modernjavaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
