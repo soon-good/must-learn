bde0a061442602c96309fed66855b0021d68dcd5
gosgjung <playmymind@daum.net>
2021-09-22 21:14:24 +0900

    스프링 카프카 정리 용도 예제 프로젝트 초기 구성 커밋

---
+++ 메시지브로커/Kafka/examples/1-spring-kafka-sample/src/main/groovy/io/study/springkafka/SpringKafkaApplication.groovy
@@ -0,0 +1,13 @@
+package io.study.springkafka
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringKafkaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringKafkaApplication, args)
+    }
+
+}
